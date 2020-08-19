2db39d3695ff2fdc459273d2ede57377ee3e4401
Vishal Ghadage <ghadagevishal993@gmail.com>
2020-08-19 23:21:59 +0530

    Update index.js

--- DB/index.js
+++ DB/index.js
@@ -5,7 +5,7 @@ var conn = mysql.createConnection({
     database: 'API',
     user: 'user',
     password: 'user',
-    host: 'localhost',
+    host: 'rajeapi.tk',
     multipleStatements: true
 })
 
