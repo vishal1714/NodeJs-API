c045403935dc1d2622d7cb2138c45177142af33b
Vishal Ghadage <ghadagevishal993@gmail.com>
2020-09-30 03:19:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var app = require("./app");
 
-var Port = process.env.PORT || 5002;
+var Port = process.env.PORT || 5001;
 // Web server start
 app.listen(Port, () => {
   console.log("Web Server is started on Port => " + Port);
